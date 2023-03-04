546250867b473d51ad4f916ba9805b15dba71590
Kunal Bhandare <kunalbhandare422@gmail.com>
2023-03-04 21:23:13 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>ShiningStar</h1>
+        <p>@All rights reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow us</h5>
+        <div>
+          <a href="https://google.com">Youtube</a>
+          <a href="https://instagram.com">Instagram</a>
+          <a href="https://github.com">Github</a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
